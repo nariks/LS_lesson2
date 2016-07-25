597787e96401422739409f8572f752ee8bade538
nariks <happycow@gmail.com>
2016-07-25 23:55:22 +1000

    Final commit with bonus features

--- rps.rb
+++ rps.rb
@@ -1,52 +1,67 @@
-VALID_CHOICES = { "r" => 'Rock',
-                  "s" => 'Scissors',
-                  "p" => 'Paper',
-                  "l" => 'Lizard',
-                  "S" => 'Spock' }
-
-WIN_COMBINATIONS = { "r" => %w(s l), #{ wins => %w(s l), 
-                                     #action => %w(crushes crushes)
-                     "s" => %w(p l),
-                     "p" => %w(r S),
-                     "l" => %w(p S),
-                     "S" => %w(r s) }
-
-#RULES = { "rs" => "crushes"
-#         "rl" => "lizard"
+GAME_RULES = { "r" => { "name"   => "Rock",
+                        "wins"   => %w(s l),
+                        "action" => %w(crushes crushes) },
+               "s" => { "name"   => "Scisscors",
+                        "wins"   => %w(p l),
+                        "action" => %w(cuts decapacitates) },
+               "p" => { "name"   => "Paper",
+                        "wins"   => %w(r sp),
+                        "action" => %w(covers disproves) },
+               "l" => { "name"   => "Lizard",
+                        "wins"   => %w(p sp),
+                        "action" => %w(eats poisons) },
+               "sp" => { "name"   => "Spock",
+                         "wins"   => %w(r s),
+                         "action" => %w(vaporizes smashes) } }
 
 def prompt(message)
   puts "\e[32m=> #{message}\e[0m"
 end
 
 def valid_choice?(input)
-  input.match(/^[rpslS]$/)
+  input.match(/^([rpsl]|(sp))$/)
 end
 
-def valid_continue?(input)
-  input.match(/^[yn]$/)
-end
-
-def select_winner(choice)
+def return_winner(choice)
   if choice['Player'] == choice['Computer']
     'Tie'
-  elsif WIN_COMBINATIONS[choice['Player']].include?(choice['Computer'])
+  elsif GAME_RULES[choice['Player']]["wins"].include?(choice['Computer'])
     'Player'
   else
     'Computer'
   end
 end
 
+def return_loser(winner)
+  winner == "Player" ? "Computer" : "Player"
+end
+
+# loser_index is used in display_win_rule method to obtain winning action word
+def return_loser_index(winner, choice)
+  loser = return_loser(winner)
+  GAME_RULES[choice[winner]]["wins"].index(choice[loser])
+end
+
+def display_win_rule(winner, choice)
+  wins_index = return_loser_index(winner, choice)
+  loser = return_loser(winner)
+  action_rule = GAME_RULES[choice[winner]]["action"][wins_index]
+  prompt("#{GAME_RULES[choice[winner]]['name']} #{action_rule} " \
+         "#{GAME_RULES[choice[loser]]['name']}")
+end
+
 def display_winner(winner, choice)
   win_result = (winner == "Tie" ? "Tie Game" : "#{winner} wins !!!")
-  prompt("Player chose #{VALID_CHOICES[choice["Player"]]} and " \
-         "Computer chose #{VALID_CHOICES[choice["Computer"]]} " \
-         "===> #{win_result}")
+  prompt("Player chose #{GAME_RULES[choice['Player']]['name']} and " \
+         "Computer chose #{GAME_RULES[choice['Computer']]['name']}.")
+  display_win_rule(winner, choice) unless winner == "Tie"
+  prompt(win_result)
 end
 
 def validate_input(player_input)
   loop do
     return player_input if valid_choice?(player_input)
-    prompt("Invalid Entry. Try again")
+    prompt("Invalid Entry. Choose from r,p,s,l,sp")
     player_input = gets.chomp
   end
 end
@@ -59,10 +74,10 @@ def display_score(score)
   printf("%-15s: %3d\n\n", 'Tie Score', score['Tie'])
 end
 
-def display_choices
+def display_selection_choices
   prompt("Valid choices:")
-  VALID_CHOICES.each do |key, value|
-    printf("   Enter %s for %-10s\n", key, value)
+  GAME_RULES.each do |key, value|
+    printf("   Enter %s for %-10s\n", key, value["name"])
   end
 end
 
@@ -75,30 +90,33 @@ score = { "Player"   => 0,
 choices = { "Player"   => "",
             "Computer" => "" }
 
+continue = true # tracks if player wants to continue playing.
+
 system("clear")
 prompt("Welcome to the game of Rock Paper Scissors Lizard Spock")
-prompt("First to reach 5 points wins")
-display_choices
+prompt("\e[31mFirst to reach 5 points wins\e[0m")
+display_selection_choices
 
-loop do
-  prompt("Enter your choice")
-  choices["Player"] = validate_input(gets.chomp)
-  choices["Computer"] = VALID_CHOICES.keys.sample
-  winner = select_winner(choices)
-  display_winner(winner, choices)
+while continue
+  loop do
+    prompt("Enter your choice")
+    choices["Player"] = validate_input(gets.chomp)
+    choices["Computer"] = GAME_RULES.keys.sample
 
-  score[winner] += 1
-  display_score(score)
+    winner = return_winner(choices)
+    display_winner(winner, choices)
 
-  max_score = [score["Player"],score["Computer"]].max
-  if max_score == 5
-    series_winner = score["Player"] > score["Computer"] ? "Player" : "Computer"
-    prompt("#{series_winner} wins series !")
-    break
-  else
-    prompt("Would you like to play again (y to continue, any key to quit)")
-    answer = gets.chomp
-    break unless answer.downcase.start_with?("y")
+    score[winner] += 1
+    display_score(score)
+
+    max_score = [score["Player"], score["Computer"]].max
+    break if max_score == 5
   end
+
+  series_winner = score["Player"] > score["Computer"] ? "Player" : "Computer"
+  prompt("#{series_winner} wins series !")
+
+  prompt("Would you like to play again (y to continue, any key to quit)")
+  continue = gets.chomp.downcase.start_with?("y")
 end
 prompt("Game over. Thanks for playing")
